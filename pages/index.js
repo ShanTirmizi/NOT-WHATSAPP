1a133e186cd59eb030a26a49209d7e17bf854cb7
Shahnawaz Tirmizi <tirmizishahnawaz@gmail.com>
2021-12-26 18:41:47 +0000

    Creating post message functionality

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,41 @@
 // pages/index.js
 import { useUser } from '@auth0/nextjs-auth0';
+import React from 'react';
 
 export default function Index() {
   const { user, error, isLoading } = useUser();
-  console.log(user.sub);
+  const [message, setMessage] = React.useState('');
+  const [messages, setMessages] = React.useState([]);
+  console.log(messages);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+
+  // create a handle submit function for a form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.message.value);
+    // setMessage(e.target.message.value);
+    setMessages([...messages, message]);
+    setMessage('');
+  };
 
   if (user) {
     return (
       <div>
         Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Send Message</button>
+        </form>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
       </div>
     );
   }
