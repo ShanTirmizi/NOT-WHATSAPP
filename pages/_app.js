d639eefea230f97f7d08ab296165a3d8bfa44701
Shahnawaz Tirmizi <tirmizishahnawaz@gmail.com>
2021-12-26 18:16:55 +0000

    added auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+// import '../styles/globals.css';
+// import { UserProvider } from '@auth0/nextjs-auth0';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <UserProvider>
+//       <Component {...pageProps} />
+//     </UserProvider>
+//   );
+// }
+
+// export default MyApp;
+
+// pages/_app.js
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
-
-export default MyApp
